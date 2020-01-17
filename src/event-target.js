7b4b7fb2c63641b2022ee5781de00181516d3a44
Marco Castiello <marco.castiello@gmail.com>
2020-01-17 13:13:04 +0000

    Avoid overriding the original EventTarget class

--- src/event-target.js
+++ src/event-target.js
@@ -1,4 +1,72 @@
 
+// Create a map of all the even handlers.
+const eventHandlerMap = new WeakMap();
+
+/**
+ * Define a class similar to the EventTarget but that can be extended.
+ * @type {ExtendableEventTarget}
+ * @class
+ */
+class ExtendableEventTarget {
+    /**
+     * Initialise the event map.
+     * @constructor
+     */
+    constructor() {
+        eventHandlerMap.set(this, {});
+    }
+
+    /**
+     * Add a listener to a specific event.
+     * @param {String} eventName
+     * @param {Function} eventCallback
+     */
+    addEventListener(eventName, eventCallback) {
+        const handlers = eventHandlerMap.get(this);
+        if (!handlers.hasOwnProperty(eventName)) {
+            handlers[eventName] = [];
+        }
+
+        if (handlers[eventName].indexOf(eventCallback) < 0) {
+            handlers[eventName].push(eventCallback);
+        }
+    }
+
+    /**
+     * Remove a registered listener to a specific event.
+     * @param {String} eventName
+     * @param {Function} eventCallback
+     */
+    removeEventListener(eventName, eventCallback) {
+        const handlers = eventHandlerMap.get(this);
+
+        if (handlers.hasOwnProperty(eventName)) {
+            const index = handlers[eventName].indexOf(eventCallback);
+
+            if (index >= 0) {
+                handlers[eventName].splice(index, 1);
+            }
+        }
+    }
+
+    /**
+     * Dispatch an event.
+     * @param {Event} event
+     */
+    dispatchEvent(event) {
+        const eventName = event.type;
+        const handlers = eventHandlerMap.get(this);
+
+        if (handlers.hasOwnProperty(eventName)) {
+            const callbacks = handlers[eventName].slice();
+
+            for (let callback of callbacks) {
+                callback(event);
+            }
+        }
+    }
+}
+    
 // Check if the Event target class exists and can be extended
 let available = false;
 if (EventTarget) {
@@ -9,68 +77,6 @@ if (EventTarget) {
 }
 
 // If not, creates a simple replacement
-if (!available) {
-    const eventHandlerMap = new WeakMap();
+window.ExtendableEventTarget = available ? window.EventTarget : ExtendableEventTarget;
 
-    class EventTarget {
-        /**
-         * Initialise the event map.
-         * @constructor
-         */
-        constructor() {
-            eventHandlerMap.set(this, {});
-        }
-
-        /**
-         * Add a listener to a specific event.
-         * @param {String} eventName
-         * @param {Function} eventCallback
-         */
-        addEventListener(eventName, eventCallback) {
-            const handlers = eventHandlerMap.get(this);
-            if (!handlers.hasOwnProperty(eventName)) {
-                handlers[eventName] = [];
-            }
-
-            if (handlers[eventName].indexOf(eventCallback) < 0) {
-                handlers[eventName].push(eventCallback);
-            }
-        }
-
-        /**
-         * Remove a registered listener to a specific event.
-         * @param {String} eventName
-         * @param {Function} eventCallback
-         */
-        removeEventListener(eventName, eventCallback) {
-            const handlers = eventHandlerMap.get(this);
-
-            if (handlers.hasOwnProperty(eventName)) {
-                const index = handlers[eventName].indexOf(eventCallback);
-
-                if (index >= 0) {
-                    handlers[eventName].splice(index, 1);
-                }
-            }
-        }
-
-        /**
-         * Dispatch an event.
-         * @param {Event} event
-         */
-        dispatchEvent(event) {
-            const eventName = event.type;
-            const handlers = eventHandlerMap.get(this);
-
-            if (handlers.hasOwnProperty(eventName)) {
-                const callbacks = handlers[eventName].slice();
-
-                for (let callback of callbacks) {
-                    callback(event);
-                }
-            }
-        }
-    }
-    
-    window.EventTarget = EventTarget;
-}
+export default ExtendableEventTarget;
