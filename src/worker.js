cf7ca82671d7e812e91abe58e0599f4f74f69b77
Marco Castiello <marco.castiello@gmail.com>
2020-01-17 18:09:34 +0000

    Added support for Blob and string code

--- src/worker.js
+++ src/worker.js
@@ -13,6 +13,15 @@ const scopeMap = new WeakMap();
 const channelMap = new WeakMap();
 
 /**
+ * Check if a URL is valid.
+ * @param {String} string
+ * @returns {Boolean}
+ */
+const isValidURL = (string) => {
+    return /^(http(s)?:\/\/.|\.\/|\/)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/g.test(string);
+};
+
+/**
  * Provide a polyfill for the Worker class. The code is not executed 
  * in a new thread but it is ran asynchronously.
  * @type {Worker}
@@ -21,14 +30,33 @@ const channelMap = new WeakMap();
 class Worker extends ExtendableEventTarget {
     /**
      * Initialise the Worker using the code downloaded from the URL.
-     * @param {String} url
+     * @param {String|Blob} urlOrBlob
      * @constructor
      */
-    constructor(url) {
+    constructor(urlOrBlob) {
         super();
         
         const scope = new WorkerScope(this);
         const channel = new WorkerChannel(scope);
+        const runCode = code => {
+            // Execute the code.
+            scope.run(code);
+
+            // Check if the code is running.
+            if (!scope.running) {
+                const event = new Event("error");
+                event.message = "Unable to execute Worker code!";
+                this.terminate();
+                this.dispatchEvent(event);
+            }
+        };
+        const notifyError = error => {
+            // Notify that it wasn't possible to load the file.
+            const event = new Event("error");
+            event.message = error;
+            this.terminate();
+            this.dispatchEvent(event);
+        };
         
         scopeMap.set(this, scope);
         channelMap.set(this, channel);
@@ -39,47 +67,43 @@ class Worker extends ExtendableEventTarget {
         this.onmessageerror = null;
         this.onerror = null;
 
-        xhr.open("GET", url);
-        xhr.onreadystatechange = () => {
-            if(xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status === 200) {
-                    // Make sure that the message callbacks executed 
-                    // when a message is posted by the worker.
-                    scope.addEventListener("message", event => {
-                        if (typeof this.onmessage === "function") {
-                            this.onmessage(event);
-                        }
-                    });
-                    scope.addEventListener("messageerror", event => {
-                        if (typeof this.onmessageerror === "function") {
-                            this.onmessageerror(event);
-                        }
-                    });
-                    this.addEventListener("error", event => {
-                        if (typeof this.onerror === "function") {
-                            this.onerror(event);
-                        }
-                    });
-                    
-                    // Execute the code.
-                    scope.run(xhr.responseText);
-                    
-                    // Check if the code is running.
-                    if (!scope.running) {
-                        const event = new Event("error");
-                        event.message = "Unable to execute Worker code!";
-                        this.terminate();
-                        this.dispatchEvent(event);
-                    }
-                } else { // Notify that it wasn't possible to load the file.
-                    const event = new Event("error");
-                    event.message = "Worker code not available!";
-                    this.terminate();
-                    this.dispatchEvent(event);
-                }
+        // Make sure that the message callbacks executed
+        // when a message is posted by the worker.
+        scope.addEventListener("message", event => {
+            if (typeof this.onmessage === "function") {
+                this.onmessage(event);
             }
-        };
-        xhr.send();
+        });
+        scope.addEventListener("messageerror", event => {
+            if (typeof this.onmessageerror === "function") {
+                this.onmessageerror(event);
+            }
+        });
+        this.addEventListener("error", event => {
+            if (typeof this.onerror === "function") {
+                this.onerror(event);
+            }
+        });
+
+        if (urlOrBlob instanceof Blob) {
+            urlOrBlob.text().then(code => runCode(code)).catch(error => notifyError(error));
+        } else if (typeof urlOrBlob === "string") {
+            if (isValidURL(urlOrBlob)) {
+                xhr.open("GET", urlOrBlob);
+                xhr.onreadystatechange = () => {
+                    if(xhr.readyState === XMLHttpRequest.DONE) {
+                        if (xhr.status === 200) {
+                            runCode(xhr.responseText);
+                        } else {
+                            notifyError("Worker code not available!");
+                        }
+                    }
+                };
+                xhr.send();
+            } else {
+                setTimeout(() => runCode(urlOrBlob), 0);
+            }
+        }
     }
 
     /**
