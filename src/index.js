3d7c1f044021c3d3ffefe8ccd41bfaacf9c07db1
Marco Castiello <marco.castiello@gmail.com>
2020-01-17 16:30:28 +0000

    Add copyright

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020
+ * Author: Marco Castiello
+ * E-mail: marco.castiello@gmail.com
+ * Project: Worker Async Polyfill
+ */
+
 import './extendable-event-target.js';
 import { Worker as PolyfillWorker } from './worker.js';
 
